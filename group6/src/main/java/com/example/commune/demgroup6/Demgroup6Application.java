751a9e746440efc448e5c7e1d81ea5ff7d4b9d5e
jasmeets <js893478@dal.ca>
2023-06-02 10:37:25 -0300

    adding basic project structure

---
+++ group6/src/main/java/com/example/commune/demgroup6/Demgroup6Application.java
@@ -0,0 +1,13 @@
+package com.example.commune.demgroup6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demgroup6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demgroup6Application.class, args);
+	}
+
+}
---
+++ group6/src/test/java/com/example/commune/demgroup6/Demgroup6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commune.demgroup6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demgroup6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
