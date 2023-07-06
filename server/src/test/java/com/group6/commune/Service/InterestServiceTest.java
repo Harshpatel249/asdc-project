a4e87b11b95e02778cec8b4f4e3df1b683caed04
mehul-247 <mh207556@dal.ca>
2023-07-06 01:12:23 -0300

    created crud api for user interests

---
+++ server/src/test/java/com/group6/commune/Service/InterestServiceTest.java
@@ -0,0 +1,16 @@
+package com.group6.commune.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InterestServiceTest {
+
+    @Test
+    void getInterestList() {
+    }
+
+    @Test
+    void addUserInterest() {
+    }
+}
