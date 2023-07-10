f93b84674393249dc011bfdd26b1fda3f42206ea
mehul-247 <mh207556@dal.ca>
2023-07-10 13:30:48 -0300

    pull from develop branch

--- server/src/main/java/com/group6/commune/Service/UserService.java
+++ server/src/main/java/com/group6/commune/Service/UserService.java
@@ -1,4 +1,18 @@
 package com.group6.commune.Service;
 
-public class UserService {
+import com.group6.commune.Model.User;
+
+public interface UserService {
+    public User getUserDetailsById(int userId);
+
+    public Boolean createUser(User user);
+
+    public Boolean updateAccountDetails(User user);
+
+    public Boolean deleteUserAccountById(int userId);
+
+    public int createVerificationCode(String email);
+
+    public Boolean updateUserPassword(User user);
+
 }
