9d7daf59ba1678f0e116cd77c67aa82238083c4b
Harsh Pranav Patel <hr979846@dal.ca>
2023-07-18 22:57:13 -0300

    Added community admin pages

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,11 @@ import Login from './pages/UserAuthentication/Login';
 import Onboarding from './pages/OnboardingPage/Onboarding';
 import CreateCommunity from './pages/Community/CreateCommunity';
 import CommunityHome from './pages/Community/CommunityHome';
+import CommunityPosts from './pages/Community/CommunityPosts';
+import CommunityEvents from './pages/Community/CommunityEvents';
+import CommunityAdminHome from './pages/CommunityAdmin/CommunityAdminHome';
+import CommunityAdminManageMembers from './pages/CommunityAdmin/CommunityAdminManageMembers';
+import CommunityAdminSettings from './pages/CommunityAdmin/CommunityAdminSettings';
 
 const router = createBrowserRouter([
   {
@@ -27,6 +32,26 @@ const router = createBrowserRouter([
       {
         path: "/community/:cid/",
         element: <CommunityHome />
+      },
+      {
+        path: "/community/:cid/posts",
+        element: <CommunityPosts />
+      },
+      {
+        path: "/community/:cid/events",
+        element: <CommunityEvents />
+      },
+      {
+        path: "/community/:cid/admin/",
+        element: <CommunityAdminHome />
+      },
+      {
+        path: "/community/:cid/admin/manage-members",
+        element: <CommunityAdminManageMembers />
+      },
+      {
+        path: "/community/:cid/admin/settings",
+        element: <CommunityAdminSettings />
       }
     ]
   },
---
+++ client/src/pages/CommunityAdmin/CommunityAdminManageMembers.js
@@ -0,0 +1,55 @@
+import { Box, Flex, Skeleton } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import CommunityAdminSideBar from '../../components/SideBar/CommunityAdminSideBar';
+
+function CommunityAdminManageMembers() {
+    const choice = 2;
+    let { cid } = useParams();
+    const [communityDetails, setCommunityDetails] = useState();
+    const [loading, setLoading] = useState(true);
+    const userid = 2;
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                const response = await fetch(`https://commune-dev-csci5308-server.onrender.com/community/${cid}`);
+
+        
+                if (response.ok ) {
+                    const responseData = await response.json();
+                    setCommunityDetails(responseData);
+
+                    setLoading(false);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [cid]); 
+
+
+    return (
+        <Flex>
+            <Flex flexGrow="1" justifyContent="center" alignItems="center" h="69vh">
+                <CommunityAdminSideBar choice={choice} />
+            </Flex>
+            <Box flexGrow="6">
+
+                {loading ? <Skeleton /> :
+                    <Flex flexDirection="column" justifyContent="start" alignItems="start">
+                        Manage Members {communityDetails.name} for {userid}
+                    </Flex>
+                }
+
+            </Box>
+        </Flex>
+    );
+}
+
+export default CommunityAdminManageMembers;
