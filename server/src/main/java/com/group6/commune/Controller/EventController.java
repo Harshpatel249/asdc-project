f6913e3d59cfc5c9afc0bd82efc3323a36226610
kbpanchal <kr946702@dal.ca>
2023-07-28 09:00:19 -0300

    Added comments in the document

--- server/src/main/java/com/group6/commune/Controller/EventController.java
+++ server/src/main/java/com/group6/commune/Controller/EventController.java
@@ -1,3 +1,7 @@
+/**
+ * This class represents the controller for managing events.
+ * Author: Kruti Panchal
+ */
 package com.group6.commune.Controller;
 
 import com.group6.commune.Exceptions.DataNotFoundException;
@@ -12,8 +16,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,22 +26,48 @@ public class EventController {
     @Autowired
     private EventServiceImpl eventService;
 
-
+    /**
+     * Retrieves all events or events matching a specific event title.
+     *
+     * @param eventTitle The optional event title to filter events.
+     * @return A ResponseEntity containing a list of Event objects and HTTP status 200 if successful.
+     * @throws DataNotFoundException If no events are found with the given event title.
+     * @author Kruti Panchal
+     */
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
-    public ResponseEntity<List<Event>> getAllEvents(@RequestParam(required = false, name = "event_title") String eventTitle){
-        if (eventTitle == null || eventTitle.isEmpty() || eventTitle.isBlank()){
+    public ResponseEntity<List<Event>> getAllEvents(@RequestParam(required = false, name = "event_title") String eventTitle) {
+        if (eventTitle == null || eventTitle.isEmpty() || eventTitle.isBlank()) {
             return ResponseEntity.ok(eventService.getAllEvents());
-        }else{
+        } else {
             return ResponseEntity.ok(eventService.getEventByName(eventTitle));
         }
     }
 
+    /**
+     * Creates a new event.
+     *
+     * @param event  The Event object containing the event data.
+     * @param result The BindingResult object for validation.
+     * @return A ResponseEntity containing the created Event object and HTTP status 201 if successful.
+     * @throws ValidationException If the input data fails validation.
+     * @author Kruti Panchal
+     */
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
-    public ResponseEntity<Event> createEvent(@RequestBody Event event, BindingResult result){
+    public ResponseEntity<Event> createEvent(@RequestBody Event event, BindingResult result) {
         return new ResponseEntity<>(eventService.createEvent(event, result), HttpStatus.CREATED);
     }
+
+    /**
+     * Retrieves an event by its ID.
+     *
+     * @param id The ID of the event to be retrieved.
+     * @return A ResponseEntity containing the retrieved Event object and HTTP status 200 if successful.
+     * @throws DataNotFoundException If the event with the given ID is not found.
+     * @author Kruti Panchal
+     *
+     */
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/{id}")
@@ -47,60 +75,152 @@ public class EventController {
         return ResponseEntity.ok(eventService.getEventById(id));
     }
 
+    /**
+     * Retrieves all events created by a specific user.
+     *
+     * @param userId The ID of the user whose created events are to be retrieved.
+     * @return A ResponseEntity containing a list of Event objects and HTTP status 200 if successful.
+     * @throws DataNotFoundException If the user has not created any events.
+     * @throws UnauthorizedAccessException If the user is not authorized to access events created by the given user.
+     * @author Kruti Panchal
+     */
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/user/{userId}")
-    public ResponseEntity<List<Event>> getUserCreatedEvents(@PathVariable int userId){
+    public ResponseEntity<List<Event>> getUserCreatedEvents(@PathVariable int userId) {
         return ResponseEntity.ok(eventService.getUserCreatedEvents(userId));
     }
 
+    /**
+     * Updates an existing event.
+     *
+     * @param event  The Event object containing the updated event data.
+     * @param result The BindingResult object for validation.
+     * @return A ResponseEntity containing the updated Event object and HTTP status 200 if successful.
+     * @throws DataNotFoundException If the event with the given ID is not found.
+     * @throws UnauthorizedAccessException If the user is not authorized to update the event.
+     * @throws ValidationException If the input data fails validation.
+     * @autho Kruti Panchal
+     */
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PutMapping
     public ResponseEntity<Event> updateEvent(@RequestBody Event event, BindingResult result){
         return ResponseEntity.ok(eventService.updateEvent(event, result));
     }
 
+    /**
+     * Deletes an event by its ID.
+     *
+     * @param id The ID of the event to be deleted.
+     * @return A ResponseEntity containing the number of events deleted and HTTP status 200 if successful.
+     * @throws DataNotFoundException If the event with the given ID is not found.
+     * @throws UnauthorizedAccessException If the user is not authorized to delete the event.
+     * @autho Kruti Panchal
+     */
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @DeleteMapping("/{id}")
     public ResponseEntity<Integer> deleteEvent(@PathVariable int id){
         return ResponseEntity.ok(eventService.deleteEvent(id));
     }
 
+    /**
+     * Adds an interest to an event.
+     *
+     * @param id          The ID of the event to which the interest will be added.
+     * @param interest_id The ID of the interest to be added.
+     * @return A ResponseEntity containing true if the addition is successful and HTTP status 200 if successful.
+     * @throws DataNotFoundException If the event or interest with the given ID is not found.
+     * @throws UnauthorizedAccessException If the user is not authorized to add interests to the event.
+     * @throws ValidationException If the input data fails validation.
+     * @autho Kruti Panchal
+     */
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping("/{id}/interests")
     public ResponseEntity<Boolean> AddEventInterest(@PathVariable int id, @RequestParam(required = true, name= "interest_id") int interest_id) {
         return new ResponseEntity<>(eventService.addEventInterests(id,interest_id), HttpStatus.CREATED);
     }
 
+    /**
+     * Retrieves all interests associated with an event by its ID.
+     *
+     * @param id The ID of the event whose interests are to be retrieved.
+     * @return A ResponseEntity containing a list of Interest objects and HTTP status 200 if successful.
+     * @throws DataNotFoundException If the event with the given ID is not found.
+     * @throws UnauthorizedAccessException If the user is not authorized to access the interests of the event.
+     * @throws ValidationException If the input data fails validation.
+     * @autho Kruti Panchal
+     */
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/{id}/interests")
     public ResponseEntity<List<Interest>> getEventInterests(@PathVariable int id) {
         return ResponseEntity.ok(eventService.getEventInterests(id));
     }
 
+    /**
+     * Deletes an interest from an event.
+     *
+     * @param id          The ID of the event from which the interest will be deleted.
+     * @param interest_id The ID of the interest to be deleted.
+     * @return A ResponseEntity containing true if the deletion is successful and HTTP status 200 if successful.
+     * @throws DataNotFoundException If the event or interest with the given ID is not found.
+     * @throws UnauthorizedAccessException If the user is not authorized to delete interests from the event.
+     * @throws ValidationException If the input data fails validation.
+     * @autho Kruti Panchal
+     */
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @DeleteMapping("{id}/interests")
     public ResponseEntity<Boolean> deleteEventInterest(@PathVariable int id, @RequestParam( required = false, name ="interest_id") int interest_id){
         return ResponseEntity.ok(eventService.deleteEventInterests(id,interest_id));
     }
 
+    /**
+     * Retrieves all events associated with a community by its ID.
+     *
+     * @param id The ID of the community whose events are to be retrieved.
+     * @return A ResponseEntity containing a list of Event objects and HTTP status 200 if successful.
+     * @throws DataNotFoundException If the community with the given ID is not found.
+     * @throws UnauthorizedAccessException If the user is not authorized to access the events of the community.
+     * @throws ValidationException If the input data fails validation.
+     * @autho Kruti Panchal
+     */
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/{id}/events")
     public ResponseEntity<List<Event>> getEventsForCommunity(@PathVariable int id){
         return ResponseEntity.ok(eventService.getEventsForCommunity(id));
     }
 
+    /**
+     * Exception handler for DataNotFoundException.
+     *
+     * @param ex The DataNotFoundException object to be handled.
+     * @return A ResponseEntity containing a message about the exception and HTTP status 404.
+     * @author Harsh Patel
+     */
     @ExceptionHandler(DataNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleDataNotFoundException(DataNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(Map.of("message", ex.getMessage()));
     }
 
+    /**
+     * Exception handler for ValidationException.
+     *
+     * @param ex The ValidationException object to be handled.
+     * @return A ResponseEntity containing a message and validation errors about the exception and HTTP status 400.
+     * @author Kruti Panchal
+     */
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<Map<String, Object>> handleValidationException(ValidationException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(Map.of("message", ex.getMessage(), "errors", ex.getErrors()));
     }
 
+    /**
+     * Exception handler for UnauthorizedAccessException.
+     *
+     * @param ex The UnauthorizedAccessException object to be handled.
+     * @return A ResponseEntity containing a message about the exception and HTTP status 401.
+     * @author Jasmeet Singh
+     */
     @ExceptionHandler(UnauthorizedAccessException.class)
     public ResponseEntity<Map<String, Object>> handleUnauthorizedAccessException(UnauthorizedAccessException ex) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
--- server/src/main/java/com/group6/commune/Controller/EventMembersController.java
+++ server/src/main/java/com/group6/commune/Controller/EventMembersController.java
@@ -1,3 +1,7 @@
+/**
+ * This class represents the controller for managing event members.
+ * Author: Kruti Panchal
+ */
 package com.group6.commune.Controller;
 
 import com.group6.commune.Exceptions.DataNotFoundException;
@@ -21,36 +25,83 @@ public class EventMembersController {
     @Autowired
     EventMemberServiceImpl eventMemberService;
 
+    /**
+     * Adds a new member to the event.
+     *
+     * @param eventMember The EventMembers object containing the member data.
+     * @param result      The BindingResult object for validation.
+     * @return A ResponseEntity containing the created EventMembers object and HTTP status 200 if successful.
+     * @throws ValidationException If the input data fails validation.
+     * @author Kruti Panchal
+     */
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
-    public ResponseEntity<EventMembers> addMember(@RequestBody EventMembers eventMember, BindingResult result){
+    public ResponseEntity<EventMembers> addMember(@RequestBody EventMembers eventMember, BindingResult result) {
         return ResponseEntity.ok(eventMemberService.addMember(eventMember, result));
     }
 
+    /**
+     * Retrieves all members of the event by event ID.
+     *
+     * @param eventId The ID of the event whose members are to be retrieved.
+     * @return A ResponseEntity containing a list of EventMembers objects and HTTP status 200 if successful.
+     * @throws DataNotFoundException If the event with the given ID is not found.
+     * @author Kruti Panchal
+     */
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
-    public ResponseEntity<List<EventMembers>> getAllMembers(@PathVariable int eventId){
-        return new ResponseEntity<>(eventMemberService.getAllMembers(eventId), HttpStatus.CREATED);
+    public ResponseEntity<List<EventMembers>> getAllMembers(@PathVariable int eventId) {
+        return new ResponseEntity<>(eventMemberService.getAllMembers(eventId), HttpStatus.OK);
     }
 
+    /**
+     * Deletes a member from the event.
+     *
+     * @param eventMember The EventMembers object containing the member data.
+     * @return A ResponseEntity containing the deleted EventMembers object and HTTP status 200 if successful.
+     * @throws DataNotFoundException If the member with the given data is not found in the event.
+     * @throws UnauthorizedAccessException If the user is not authorized to delete the member from the event.
+     * @autho Kruti Panchal
+     */
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @DeleteMapping
-    public ResponseEntity<EventMembers> deleteMember(@RequestBody EventMembers eventMember){
+    public ResponseEntity<EventMembers> deleteMember(@RequestBody EventMembers eventMember) {
         return ResponseEntity.ok(eventMemberService.deleteMember(eventMember));
     }
 
+    /**
+     * Exception handler for DataNotFoundException.
+     *
+     * @param ex The DataNotFoundException object to be handled.
+     * @return A ResponseEntity containing a message about the exception and HTTP status 404.
+     * @author Kruti Panchal
+     */
     @ExceptionHandler(DataNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleDataNotFoundException(DataNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(Map.of("message", ex.getMessage()));
     }
 
+    /**
+     * Exception handler for ValidationException.
+     *
+     * @param ex The ValidationException object to be handled.
+     * @return A ResponseEntity containing a message and validation errors about the exception and HTTP status 400.
+     * @author Kruti Panchal
+     */
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<Map<String, Object>> handleValidationException(ValidationException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(Map.of("message", ex.getMessage(), "errors", ex.getErrors()));
     }
 
+    /**
+     * Exception handler for UnauthorizedAccessException.
+     *
+     * @param ex The UnauthorizedAccessException object to be handled.
+     * @return A ResponseEntity containing a message about the exception and HTTP status 401.
+     * @author Jasmeet Singh
+     */
     @ExceptionHandler(UnauthorizedAccessException.class)
     public ResponseEntity<Map<String, Object>> handleUnauthorizedAccessException(UnauthorizedAccessException ex) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
